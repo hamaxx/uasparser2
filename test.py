40aae9eef881ede36690209eab214855cc727bb8
Jure Ham <jure@hamsworld.net>
2012-12-09 20:16:12 +0100

    simple cache

--- test.py
+++ test.py
@@ -1,25 +1,39 @@
 import json
 import time
+import random
 from uasparser2 import UASparser
 
-test_uas = json.load(open('uas.json', 'r'))
+in_uas = json.load(open('uas.json', 'r'))
 
 t0 = time.time()
 up = UASparser()
 print 'load:', time.time() - t0
 
-t0 = time.time()
-for _ in xrange(10):
-	print '.',
-	for uas, obj in test_uas.iteritems():
-		try:
-			new_obj = up.parse(uas)
+test_uas = []
 
-			assert new_obj == obj
-		except:
-			print
-			for k, v in obj.iteritems(): print k, '\t', v, '\t', new_obj[k]
-			raise
+random.seed(123)
+test_list = in_uas.items()
+random.shuffle(test_list)
+
+for s in xrange(1, 2000):
+	limit = max(int(float(len(test_list)) / (s ** 0.9)), 1)
+	for i in xrange(0, limit):
+		ua = test_list[i]
+		test_uas.append(ua)
+
+random.seed(321)
+random.shuffle(test_uas)
+
+t0 = time.time()
+for uas, obj in test_uas:
+	try:
+		new_obj = up.parse(uas)
+
+		assert new_obj == obj
+	except:
+		print
+		for k, v in obj.iteritems(): print k, '\t', v, '\t', new_obj[k]
+		raise
 
 print
 print 'parse:', time.time() - t0
