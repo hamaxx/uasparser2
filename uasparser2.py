6d3f440b6f565969566decd721f25ef38f0a12b3
Jure Ham <jure@hamsworld.net>
2012-12-10 00:39:06 +0100

    readme fix

--- uasparser2.py
+++ uasparser2.py
@@ -11,9 +11,9 @@ Based on:
 	Modified by Michal Molhanec http://molhanec.net
 
 Usage:
-	from uasparser import UASparser
+	from uasparser2 import UASparser
 
-	uas_parser = UASparser('/path/to/your/cache/folder')
+	uas_parser = UASparser('/path/to/your/cache/folder', mem_cache_size=1000)
 
 	result = uas_parser.parse('YOUR_USERAGENT_STRING')
 """
