11e61a08e00adef1b84a5a088debb871e23519b5
Jure Ham <jure@hamsworld.net>
2013-03-01 14:25:37 -0800

    Convert to installable package.

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+setup(name='uasparser2',
+      version='0.1',
+      description="Fast and reliable User-Agent parser for Python.",
+      url='https://github.com/mailgun/uasparser2',
+      packages=find_packages(),
+      zip_safe=True)
