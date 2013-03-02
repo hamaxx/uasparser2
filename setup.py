405e6ac576bf52df33f7c3e3cb2721675b63c205
Jure Ham <jure@hamsworld.net>
2013-03-02 20:46:25 +0100

    changed url in setup.py to this repository

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(name='uasparser2',
       version='0.1',
       description="Fast and reliable User-Agent parser for Python.",
-      url='https://github.com/mailgun/uasparser2',
+      url='https://github.com/hamaxx/uasparser2',
       packages=['uasparser2',],
       zip_safe=True)
